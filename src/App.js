83a3f491825379620c248cbd2963aa3a1b16dc98
Yuriy <ykarkavchuk@gmail.com>
2021-09-24 08:37:45 +0300

    nav push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
 import Main from './components/main/main';
+<<<<<<< Updated upstream
 import Footer from './components/footer/footer';
+=======
+>>>>>>> Stashed changes
 import Nav from './components/nav/nav';
 
 
 function App() {
   return (
     <>
+<<<<<<< Updated upstream
       <Nav />
       <Main />
       <Footer />
     </>
   ); 
 
+=======
+    <Nav />
+    <Main />
+    </>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App;
---
+++ src/components/nav/nav.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import navbar from './nav.module.css';
+import logo from './images/react.png';
+
+  
+  export default function Nav() {
+      return (
+        <div className={navbar.menu_outer}>
+          <div className={navbar.alignleft}>
+            <div className ={navbar.logo}>
+                <a href="#">
+                <img title="logo" src={logo} alt="logo" wight = "70" height ="70"/>
+                </a>
+            </div>
+          </div>
+          <div className={navbar.navcontainer}>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Catalog</a>
+              </li>
+              <li>
+                <a href="#">Cart</a>
+              </li>
+            </ul>
+          </div>
+          
+        </div>
+      )
+      }
+  
+    
+     
+    
