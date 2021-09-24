5b11060762e47d2c52c4cf8b2e58a040e0b883ea
marian muzyka <64384450+mrjnmzk@users.noreply.github.com>
2021-09-24 08:33:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Main from './components/main/main';
 import Footer from './components/footer/footer';
-// import Nav from './components/nav/nav';
+import Nav from './components/nav/nav';
 
 
 function App() {
   return (
     <>
-      {/* <Nav /> */}
+      <Nav />
       <Main />
       <Footer />
     </>
