2d918067021691a6b09172dd12e6908a6335d4bf
marian muzyka <64384450+mrjnmzk@users.noreply.github.com>
2021-09-24 08:07:31 +0300

    merge footer & main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Main from './components/main/main';
 import Footer from './components/footer/footer';
+// import Nav from './components/nav/nav';
 
 
 function App() {
   return (
     <>
+      {/* <Nav /> */}
       <Main />
       <Footer />
     </>
