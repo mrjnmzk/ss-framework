71e750deda8757d31fe788ab0a6433217c31fb8b
marian muzyka <64384450+mrjnmzk@users.noreply.github.com>
2021-09-24 08:45:14 +0300

    stash

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import './App.css';
 import Main from './components/main/main';
-<<<<<<< Updated upstream
 import Footer from './components/footer/footer';
-=======
->>>>>>> Stashed changes
 import Nav from './components/nav/nav';
 
 
 function App() {
   return (
     <>
-<<<<<<< Updated upstream
       <Nav />
       <Main />
       <Footer />
     </>
   ); 
 
-=======
-    <Nav />
-    <Main />
-    </>
-  );
->>>>>>> Stashed changes
 }
 
 export default App;
