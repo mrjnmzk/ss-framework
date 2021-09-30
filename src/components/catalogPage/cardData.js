7c7906991271ecdf18ad71bb370cc283ecede35b
Oleksandr Sovynskyi <oleksandrsov@gmail.com>
2021-09-30 14:45:29 +0300

    Pageitem

--- src/components/catalogPage/cardData.js
+++ src/components/catalogPage/cardData.js
@@ -4,32 +4,36 @@ const CardData = [
         imgUrl: "https://images.pexels.com/photos/6311652/pexels-photo-6311652.jpeg?cs=srgb&dl=pexels-monstera-6311652.jpg&fm=jpg",
         title: "T-Shirt",
         subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry..",
-        price: "100 UAH" 
-        
+        price: "100 UAH", 
+        sex: "female",
+        size: "L"
     },
     {
         id: 2,
         imgUrl: "https://images.pexels.com/photos/2529157/pexels-photo-2529157.jpeg?cs=srgb&dl=pexels-melvin-buezo-2529157.jpg&fm=jpg",
         title: "Shoes",
         subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry..",
-        price: "1000 UAH" 
-        
+        price: "1000 UAH", 
+        sex: "female",
+        size: "XL"
     },
     {
         id: 3,
         imgUrl: "https://images.pexels.com/photos/4210866/pexels-photo-4210866.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4210866.jpg&fm=jpg",
         title: "Pants",
         subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry..",
-        price: "700 UAH" 
-        
+        price: "700 UAH", 
+        sex: "male",
+        size: "M"
     },
     {
         id: 4,
         imgUrl: "https://images.pexels.com/photos/5704187/pexels-photo-5704187.jpeg?cs=srgb&dl=pexels-anna-khomutova-5704187.jpg&fm=jpg",
         title: "Warm",
         subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry..",
-        price: "400 UAH" 
-        
+        price: "400 UAH", 
+        sex: "male",
+        size: "S"
     }
 ]
 
--- src/components/catalogPage/pageItem.js
+++ src/components/catalogPage/pageItem.js
@@ -1,21 +1,18 @@
 import React from 'react';
+import CatalogProduct from './catalogPage';
 import CardData from './cardData';
-import styles from './catalogPage.module.css';
-import Filter from './filter';
-import PageItem from './pageItem';
-import { Link } from 'react-router-dom';
 
-
-
-export default function CatalogProduct() {
+export default function PageItem() {
+    
     return (
         <>
-            <Filter />
            
-        <ul className={styles.container}>
+        <ul>
             {CardData.map(val => {
+                
                 return (
-                    <li className={styles.card} key={val.id}>
+                    
+                    <li key={val.id}>
                                 <img src={val.imgUrl} alt="card" width="100" height="150" />
                                 <h2>{val.title}</h2>
                                 <p>{val.subtitle}</p>
@@ -23,22 +20,16 @@ export default function CatalogProduct() {
                                 <p>Size: {val.size}</p>
                                 <p>Price: {val.price}</p>
                                
-                                <button  value={val.id} className={styles.btn}>
-                                <Link to="/product"  className={styles.btn_text}>
-                                    View more</Link>  
-                                </button>
+                           
                                 
                             </li>
                         )
                     }
+                
                 )
+            
             }
             </ul>
         </>    
     )
 }
-
-
-
- 
-
